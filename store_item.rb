33b092183db675ef1eedae57ae9dc0baa771d9cd
Kelly Scheffler <kmscheffler@gmail.com>
2017-07-24 20:04:38 -0500

    create hashes

---
+++ store_item.rb
@@ -0,0 +1,9 @@
+shoes1 = {:color => "red", :price => 50, :gender => "womens", :size => 8}
+shoes2 = {:color => "teal", :price => 65, :gender => "womens", :size => 7}
+shoes3 = {color: "gray", price: 55, gender: "mens", size: 12}
+
+p "We have #{shoes1[:gender]} shoes in #{shoes1[:color]} size #{shoes1[:size]} for $ #{shoes1[:price]}"
+
+p "We have #{shoes2[:gender]} shoes in #{shoes2[:color]} size #{shoes2[:size]} for $ #{shoes2[:price]}"
+
+p "We have #{shoes3[:gender]} shoes in #{shoes3[:color]} size #{shoes3[:size]} for $ #{shoes3[:price]}"
